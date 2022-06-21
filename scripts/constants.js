31e06cb8d9f7b9bdf0b1b9f90ddd7dfd84355e9b
yingpengsha <yingpengsha@kuaishou.com>
2022-06-21 22:03:53 +0800

    refactor

--- scripts/constants.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  MAIN_ROOT: path.resolve(__dirname, '../src/main'),
-  MAIN_PRELOAD_ROOT: path.resolve(__dirname, '../src/preload'),
-  RENDERER_ROOT: path.resolve(__dirname, '../src/renderer')
-}
