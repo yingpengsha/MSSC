01297df45df92ff6587283d37013856ac6919234
yingpengsha <yingpengsha@kuaishou.com>
2022-06-28 15:43:32 +0800

    fix RaceEthnicity valuemap

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
 export const Sex = {
   Male: 1,
-  Female: 2
-}
+  Female: 2,
+};
 
 export const RaceEthnicity = {
-  Black: 1,
-  Hispanic: 2,
-  White: 3
-}
+  Hispanic: 1,
+  Black: 2,
+  White: 3,
+};
